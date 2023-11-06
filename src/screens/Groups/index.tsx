e70ebd02d75bf4f363482361d888c2ca87eacdf3
Aron Adams <aronrapetto@hotmail.com>
2023-11-06 12:52:36 -0300

    feat: create app theme

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Title } from './styles'
+import {Container, Title} from './styles'
 
 export function Groups() {
   return (
     <Container>
-      <Title>Groups</Title>
+      <Title>
+        Groups
+      </Title>
     </Container>
   );
 }
