93aa30a7f696ff66bb1639924ed3a742d0723458
Aron Adams <aronrapetto@hotmail.com>
2023-11-08 11:19:39 -0300

    feat: Loading component

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Groups } from "@screens/Groups";
+import { Loading } from '@components/Loading';
 import theme from './src/theme' 
 
 export default function App() {
@@ -12,9 +12,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {
-        fontsLoaded ? <Groups /> : <ActivityIndicator />
-      }
+      { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
